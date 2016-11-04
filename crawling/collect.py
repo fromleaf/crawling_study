f2304b2cac66d885a010bcfac4cbb42a9156820b
yoon <fromleaf@gmail.com>
2016-11-04 14:40:45 +0900

    add collect features
    1. by requests lib
    2. by feedparser lib
    3. robobrowser lib

--- crawling/collect.py
+++ crawling/collect.py
@@ -1,13 +1,67 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import feedparser
+
+from robobrowser import RoboBrowser
 
 
-def get_data_by_requests():
-    url = 'http://fromleaf.tistory.com'
+def by_requests(url):
     result = requests.post(url)
     html = result.text
 
     print(html)
 
 
+def by_robobrowser(url):
+    browser = RoboBrowser(history=True)
+    browser.open(url)
+    print(str(browser.select))
+
+
+def login_by_robobrowser(url):
+    browser = RoboBrowser(history=True)
+    browser.open(url)
+    form = browser.get_form(action='/login/')
+
+    form['username'] = 'username'
+    form['password'] = 'password'
+    browser.session.header['Referrer'] = url # for CSRF
+
+    browser.submit_form(form)
+
+
+def funny_by_robobrowser(url):
+    browser = RoboBrowser(history=True)
+    browser.open(url)
+
+    songs = browser.select('.song_link')
+    browser.follow_link(songs[0])
+    lyrics = browser.select('.lyrics')
+    lyrics[0].text
+
+    # back to results page
+    browser.back()
+
+    # Look up my favorite song
+    song_link = browser.get_link('trains')
+    browser.follow_link(song_link)
+
+
+def by_feedparser():
+    raw_data = """
+        <rss version="2.0">
+        <channel>
+        <title>Sample Feed</title>
+        </channel>
+        </rss>
+    """
+    d = feedparser.parse(raw_data)
+    print(d)
+    print(d['feed']['title'])
+
+
+def get_news_by_feedparser(url):
+    d = feedparser.parse(url)
+    print(d)
+    print(d['feed'])
--- main.py
+++ main.py
@@ -3,4 +3,9 @@
 from crawling import collect
 
 if __name__ == '__main__':
-    collect.get_data_by_requests()
+    # collect.by_requests('http://fromleaf.tistory.com')
+    # collect.by_robobrowser('http://fromleaf.tistory.com')
+    # collect.by_feedparser()
+    collect.get_news_by_feedparser(
+        'http://imnews.imbc.com/news/2016/politic/article/4155019_19803.html'
+    )
